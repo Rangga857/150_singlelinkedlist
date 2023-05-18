f66692c093f3a6b2d50e89f15bd9a5525e0b7e19
Rangga857 <azharrangga28@gmail.com>
2023-05-18 21:17:27 +0700

    membuat void searchData

--- singlelinkedlist/singlelinkedlist.cpp
+++ singlelinkedlist/singlelinkedlist.cpp
@@ -117,5 +117,14 @@ void searchData() {
 		int nim;
 		cout << "Masukkan NIM : ";
 		cin >> nim;
+		Node* currentNode = START;
+		while (currentNode != NULL) {
+			if (currentNode->noMhs == nim) {
+				cout << "NIM : " << currentNode->noMhs << ", Nama" << currentNode->name << endl;
+				return;
+				}
+			currentNode = currentNode->next;
+		}
+		cout << "Data tidak ditemukan" << endl;
 	}
 }
